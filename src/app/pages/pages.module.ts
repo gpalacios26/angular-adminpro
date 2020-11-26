fb6d1e4dcfc5461e58a43312bb1c5d3bb565ca0b
Gregory Palacios <gregpalacios26@gmail.com>
2020-11-26 11:41:30 -0500

    Agregando servicios y rutas sidebar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,13 +10,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     CommonModule,
@@ -30,7 +32,8 @@ import { PagesComponent } from './pages.component';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ]
 })
 export class PagesModule { }
