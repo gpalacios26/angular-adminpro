069d18ca7e5a199be9136b9a87a8290e3b17f333
Gregory Palacios <gregpalacios26@gmail.com>
2020-12-10 19:58:14 -0500

    Agregando lazy load a las rutas

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private usuarioService: UsuarioService,
     private router: Router
   ) { }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+    return this.usuarioService.validarToken()
+      .pipe(
+        tap(esAutenticado => {
+          if (!esAutenticado) {
+            this.router.navigateByUrl('/login');
+          }
+        })
+      )
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
---
+++ src/app/pages/child-routes.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminGuard } from '../guards/admin.guard';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
+import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
+import { MedicoComponent } from './mantenimientos/medicos/medico.component';
+
+const childRoutes: Routes = [
+  { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+  { path: 'main', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+  { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress Bar' } },
+  { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráficas' } },
+  { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+  { path: 'rxjs', component: RxjsComponent, data: { titulo: 'Rxjs' } },
+  { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de Usuario' } },
+  { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de Cuenta' } },
+  { path: 'usuarios', component: UsuariosComponent, canActivate: [AdminGuard], data: { titulo: 'Mantenimiento de Usuarios' } },
+  { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de Hospitales' } },
+  { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de Médicos' } },
+  { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Mantenimiento de Médicos' } }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(childRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class ChildRoutesModule { }
